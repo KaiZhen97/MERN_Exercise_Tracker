89bc12fbf83977e1c4595746132273a17e09bb34
KaiZhen97 <kaizhen970123@gmail.com>
2022-09-21 23:54:37 +0800

    update

---
+++ backend/models/exercise.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const Schme = mongoose.Schme;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+module.exports = Exercise;
